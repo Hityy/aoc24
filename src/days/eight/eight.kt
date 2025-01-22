9fd73c81e9e93a80f613ffb44706a04fea7e06fe
Michał Hiterski <michal.hiterski@comarch.com>
2025-01-22 13:16:02 +0100

    eight day

--- src/days/eight/eight.kt
+++ src/days/eight/eight.kt
@@ -1,57 +1,50 @@
 package days.eight
-import days.five.isPageListCorrect
 import days.six.column
 import days.six.row
 import java.io.File
-import kotlin.math.abs
 
 typealias Point = Pair<Int,Int>
 typealias Vector = Pair<Int,Int>
 
-class PointC(val row: Int, val column: Int, type: Char) {}
-
+data class Node(val point: Point,val type: Char) {}
+// 338 too high
+// 332
 fun solveEighthDayFirstStar() {
     val grid =
-        File("src/days/eight/test1.txt")
+        File("src/days/eight/src1.txt")
             .readLines()
             .map { it.split("").filter { it != "" }.map { it.first()} }
-
+    
     val antennas  = findInGrid(grid) { it != '.' && it != '#'}
-    val antiNodes = findInGrid(grid) { it == '#'}
-    println("antennas $antennas")
-//    println(antiNodes)
-//    val pointsPair = antennas[0] to antennas[1]
-//    val differenceVector = calculateDifferenceVector(antennas[0] to antennas[1])
-//    val antiNodesPair = calculateAnitNodesCoordinates(pointsPair,differenceVector)
-//    println(antiNodesPair)
-
-//    val pairwise = collectAllPairwise(listOf(1,2,3,4))
-//    println(pairwise)
-    println("all expected antinodes = $antiNodes")
-
-    val allPairwisePoints = collectAllPairwisePoints(antennas)
-//    println(allPairwisePoints.size)
-//    println(allPairwisePoints)
-
-    val res = allPairwisePoints
-        .flatMap {
+    val antennasGroups = antennas.groupBy { it.type }
+    val allPairwiseGroupPoints = collectAllPairwisePoints(antennasGroups)
+    var res = allPairwiseGroupPoints
+        .mapValues { (_, points) -> points.flatMap {
             val diff = calculateDifferenceVector(it)
             calculateAnitNodesCoordinates(it,diff).toList()
-        }
+        } }
+        .values.flatten()
         .filter { isPointInGrid(grid,it) }
         .distinct()
 
-    println("Result: $res")
+    println("Result size: ${res.size}")
+
 }
 
 fun isPointInGrid(grid: List<List<Char>>, point: Point) =
-    point.row in 0 .. grid.size && point.column in 0 .. grid[0].size
+    point.row in 0 until grid.size && point.column in 0 until grid[0].size
 
 
-fun findInGrid(grid: List<List<Char>>, isChar: (c: Char) -> Boolean): List<Point> =
+fun findInGrid(grid: List<List<Char>>, isChar: (c: Char) -> Boolean): List<Node> =
      grid.foldIndexed(mutableListOf()) { row, acc, cur ->
-        acc += cur.mapIndexedNotNull { column, char -> if(isChar(char))  row to column else null }
+        acc += cur.mapIndexedNotNull { column, char ->
+            if(isChar(char))
+                Node(row to column,char)
+            else
+                null
+        }
         acc }
+
 
 fun calculateDifferenceVector(points: Pair<Point,Point>): Vector {
     val (point1,point2) = points
@@ -69,24 +62,18 @@ fun calculateAnitNodesCoordinates(points: Pair<Point,Point>, differenceVector: V
     return  (row1 - dr to column1 - dc) to (row2 + dr to column2 + dc)
 }
 
-fun collectAllPairwisePoints(points: List<Point>): List<Pair<Point,Point>> =
-    points.flatMapIndexed() { index, point1 -> points.drop(index +1 ).map { point1 to it } }
+fun collectAllPairwisePoints(points: Map<Char,List<Node>>): Map<Char,List<Pair<Point,Point>>> =
+    points.mapValues { (type,list) ->
+        list.flatMapIndexed() { index, node ->
+            list.drop(index +1 ).map { node.point to it.point }
+        }
+    }
 
 fun collectAllPairwise(list: List<Int>) : List<Pair<Int,Int>> =
     list.flatMapIndexed { index, first -> list.drop(index + 1).map { first to it } }
 
 
-//fun findAntenas(grid: List<List<Char>>): List<Pair<Int,Int>> {
-//    return grid.foldIndexed(mutableListOf()) { row, acc, cur ->
-//        acc += cur.mapIndexedNotNull { column, char -> if(char !=  '.' && char != '#')  row to column else null }
-//        acc
-//    }
-//}
-//
-//fun findAntinodes(grid: List<List<Char>>): List<Pair<Int,Int>> {
-//    return grid.foldIndexed(mutableListOf()) { row, acc, cur ->
-//        acc += cur.mapIndexedNotNull { column, char -> if(char == '#')  row to column else null }
-//        acc
-//    }
-//}
-
+//    val antiNodes = findInGrid(grid) { it == '#'}
+//    val expectedAntinodes = antiNodes.map { it.point }
+//    println("all expected antinodes = $expectedAntinodes")
+//    println("all expected antinodes size = ${expectedAntinodes.size}")
