21bf9be7dad0e1a1a92cdfa909cd155acf5561ce
Michał Hiterski <michal.hiterski@comarch.com>
2025-02-03 22:36:55 +0100

    (15) * test1 passed

--- src/days/fifteen/fifteen.kt
+++ src/days/fifteen/fifteen.kt
@@ -2,13 +2,14 @@ package days.fifteen
 
 import column
 import row
+import java.io.File
 
 val robot = '@'
 val box = 'O'
 val wall = '#'
 val empty = '.'
 
-private typealias Point = Pair<Int,Int>
+private typealias Point = Pair<Int, Int>
 private typealias Grid = List<List<Char>>
 private typealias MutableGrid = List<MutableList<Char>>
 private typealias Row = Int
@@ -20,18 +21,20 @@ val right = 0 to 1
 val down = 1 to 0
 val left = 0 to -1
 
-val diections = mapOf<Char,Direction>(
+val directions = mapOf<Char, Direction>(
     '<' to left,
     'v' to down,
     '>' to right,
     '^' to up
 )
 
-private operator fun Pair<Int,Int>.plus(dir: Direction): Point {
+private operator fun Pair<Int, Int>.plus(dir: Direction): Point {
     return Point(this.row + dir.row, this.column + dir.column)
 }
 
-private fun <T> include(grid: List<List<T>>, point: Point): Boolean = point.row in grid.indices && point.column in grid[0].indices
+private fun <T> include(grid: List<List<T>>, point: Point): Boolean =
+    point.row in grid.indices && point.column in grid[0].indices
+
 private fun include(grid: Array<BooleanArray>, point: Point): Boolean = grid[point.row][point.column] ?: false
 
 private operator fun <T> List<List<T>>.contains(point: Point) = include(this, point)
@@ -40,22 +43,62 @@ private operator fun Array<BooleanArray>.contains(point: Point) = include(this, 
 
 fun solveFifteenDayFirstStar() {
     tests()
+
+    val (grid, moves ) = getSource("src/days/fifteen/test.txt")
+
+//    val robot = findRobotPosition(grid)!!
+
+//    grid.forEach{ println(it) }
+//    println("\n")
+//    val n = move(grid,robot,'^')
+//    println(robot)
+//    println(n)
+//    grid.forEach{ println(it) }
+
+//    println("\n")
+//    move(grid,robot,'^')
+//    grid.forEach{ println(it) }
+//    for (move in moves) {
+//
+//    }
+
+    var robot = findRobotPosition(grid)!!
+    grid.forEach{ println(it) }
+    for(m in moves) {
+        val n = move(grid,robot,m)
+        if(n != null) {
+            robot = n
+        }
+        println("\n")
+        println("move: $m")
+        println("ACTUAL: $n")
+        println("new robot position $robot")
+        grid.forEach{ println(it) }
+    }
 }
 
+fun getSource(path: String): Pair<List<MutableList<Char>>, List<Char>> {
+    val lines = File(path).readLines()
+    val emptyline = lines.indexOf("")
+    val grid = lines.subList(0, emptyline).map { it.toCharArray().toMutableList() }
+    val moves = lines.subList(emptyline + 1, lines.size).flatMap { it.toCharArray().toList() }
+    return grid to moves
+}
 
 fun tests() {
-    horizontalShiftTest("#..O@..O.#",1,'<')
-    horizontalShiftTest("#.OO@..O.#",2,'<')
-    horizontalShiftTest("#.OOO@.O.#",3,'<')
-    horizontalShiftTest("#.#OO@.O.#",4,'<')
-    horizontalShiftTest("#.#OO#@.O.#",5,'<')
+    horizontalShiftTest("#..O@..O.#", "#.O@...O.#",1, '<')
+    horizontalShiftTest("#.OO@..O.#","#OO@...O.#", 2, '<')
+    horizontalShiftTest("#.OOO@.O.#", "#OOO@..O.#",3, '<')
+    horizontalShiftTest("#.#OO@.O.#","#.#OO@.O.#" ,4, '<')
+    horizontalShiftTest("#.#OO#@.O.#","#.#OO#@.O.#", 5, '<')
+    horizontalShiftTest("#.#OO.@OO.#","#.#OO..@OO#", 6, '>')
 
 
-    verticalShiftTest("#..O@..O.#",1,'^')
-    verticalShiftTest("#.OO@..O.#",2,'^')
-    verticalShiftTest("#.OOO@.O.#",3,'^')
-    verticalShiftTest("#.#OO@.O.#",4,'^')
-    verticalShiftTest("#.#OO#@.O.#",5,'^')
+    verticalShiftTest("#..O@..O.#", 1, '^')
+    verticalShiftTest("#.OO@..O.#", 2, '^')
+    verticalShiftTest("#.OOO@.O.#", 3, '^')
+    verticalShiftTest("#.#OO@.O.#", 4, '^')
+    verticalShiftTest("#.#OO#@.O.#", 5, '^')
 }
 
 //sealed  class TestDirection {
@@ -67,14 +110,28 @@ fun translateXY(testGrid: String): List<MutableList<Char>> {
     return testGrid.toList().map { mutableListOf(it) }
 }
 
-fun horizontalShiftTest(testGrid: String,n: Int, dir: Char) {
+fun horizontalShiftTest(testGrid: String,expected: String, n: Int, dir: Char) {
     println("\n")
     println("CASE $n)")
     val list = testGrid.toMutableList()
-    val robot = 0 to list.indexOf('@')
-    println(list)
-    move(listOf(list),robot,dir)
-    println(list)
+    val robotPoint = 0 to list.indexOf('@')
+    println(list.joinToString(""))
+    move(listOf(list), robotPoint, dir)
+    println(list.joinToString(""))
+
+//    val newRobotPoint = list.indexOf(robot)
+//    val dp = directions[dir]!!
+
+//    val expectedPoint = robotPoint + dp
+//    if( expectedPoint != 0 to newRobotPoint ) {
+//        throw Error("CASE $n FAILED WRONG ROBOT POSITION EXPTECTED: $expectedPoint GOT: ${0 to newRobotPoint}")
+//    }
+
+    if(expected != list.joinToString("")) {
+        throw Error("CASE $n FAILED")
+    }
+
+
 }
 
 fun verticalShiftTest(testGrid: String, n: Int, dir: Char) {
@@ -84,54 +141,64 @@ fun verticalShiftTest(testGrid: String, n: Int, dir: Char) {
     val column = translateXY(testGrid)
     var row = -1
 
-    for(index in column.indices) {
-        if(column[index][0] == '@'){
+    for (index in column.indices) {
+        if (column[index][0] == '@') {
             row = index
         }
     }
 
     val columnOriginal = translateXY(testGrid)
-    move(column,row to 0,dir)
-    column.forEachIndexed { index,list -> println(" ${columnOriginal[index][0]}  ${list[0]}") }
+    move(column, row to 0, dir)
+    column.forEachIndexed { index, list -> println(" ${columnOriginal[index][0]}  ${list[0]}") }
 }
 
-fun move(grid: MutableGrid, position: Point, move: Char,parent: Char = '.'): Point? {
-    val dir = diections[move]!!
+fun move(grid: MutableGrid, position: Point, move: Char, parent: Char = '.'): Point? {
+    val dir = directions[move]!!
     val nextPoint = position + dir
 
-    if(nextPoint !in grid) {
-        return null
+    if (nextPoint !in grid) {
+        return position
     }
 
     val nextChar = grid[nextPoint.row][nextPoint.column]
+    val currentChar = grid[position.row][position.column]
 
-    if(nextChar == wall) {
+//    println(nextChar)
+
+    if (nextChar == wall) {
+//        if(currentChar == robot) {
+//            return position
+//        }
         return null
     }
 
-    val currentChar = grid[position.row][position.column]
-    if(nextChar == box) {
-        val (row,column) = move(grid,nextPoint,move,currentChar) ?: return null
+    if (nextChar == box) {
 
-        if(currentChar == robot) {
+        val (row, column) = move(grid, nextPoint, move, currentChar) ?: return null
+
+        if (currentChar == robot) {
             grid[position.row][position.column] = '.'
-            grid[row][column]='@'
+            grid[row][column] = '@'
+            return nextPoint
         }
+        println("DASDAD")
         return position
     }
 
 
     grid[position.row][position.column] = parent
     grid[nextPoint.row][nextPoint.column] = currentChar
+    if(currentChar == robot) {
+        return nextPoint
+    }
     return position
 }
 
 
-
 fun findRobotPosition(grid: Grid): Point? {
-    for(row in grid.indices) {
-        for(column in grid[row].indices) {
-            if(grid[row][column] == robot) {
+    for (row in grid.indices) {
+        for (column in grid[row].indices) {
+            if (grid[row][column] == robot) {
                 return row to column
             }
         }
