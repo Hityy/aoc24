3e559257f96ab732cdacf72e72c6b33f3887fc96
Michał Hiterski <michal.hiterski@comarch.com>
2025-02-18 22:03:26 +0100

    (18) ** done

--- src/days/eightteen/eightteen.kt
+++ src/days/eightteen/eightteen.kt
@@ -1,4 +1,4 @@
-package days.eightteen
+package days.eighteen
 
 import column
 import row
@@ -8,46 +8,68 @@ import java.util.*
 typealias Point = Pair<Int, Int>
 typealias Grid = Array<Array<Char>>
 
-fun solveEightteenDayFirstStar() {
-    val bytesSeq = getBytes("src1").take(1024)
+fun solveEighteenDayFirstStar() {
+    //    tests()
+    val input = getInput("src1")
+    val bytesSeq = getBytes(input).take(1024)
     val grid = getGrid(71)
-    for(byte in bytesSeq) {
-        val (row,column) = byte
+    for (byte in bytesSeq) {
+        val (row, column) = byte
         grid[row][column] = '#'
     }
-    val path = dijistra(grid) ?: throw Exception("No path found")
+    val path = dijkstra(grid) ?: throw Exception("No path found")
     println(path.size - 1)
-//    tests()
 }
 
-fun solveEightteenDaySecondStar() {
+fun solveEighteenDaySecondStar() {
     val result = binarySearch()
     println(result)
 }
 
-fun arrangeBytesInMemoryFor(n: Int): Pair<Boolean,Point> {
-    val bytesSeq = getBytes("src1").take(n)
+fun binarySearch(): Point {
+    val input = getInput("src1")
+    var found: Point = -1 to -1
+    var max = 5000
+    var min = 0
+    var middle = 0
+
+    while (max - min > 1) {
+        middle = (max + min) / 2
+        val (result, lastPoint) = arrangeBytesInMemoryFor(middle, input)
+        found = lastPoint
+        if (result) {
+            min = middle
+        } else {
+            max = middle
+        }
+    }
+
+    return found
+}
+
+fun arrangeBytesInMemoryFor(n: Int, input: List<String>): Pair<Boolean, Point> {
+    val bytesSeq = getBytes(input).take(n)
     val grid = getGrid(71)
     var lastByte: Point = 0 to 0
-    for(byte in bytesSeq) {
-        val (row,column) = byte
+    for (byte in bytesSeq) {
+        val (row, column) = byte
         grid[row][column] = '#'
         lastByte = row to column
     }
 
-    val path = dijistra(grid)
-    if(path != null) {
-        return true to lastByte
+    val path = dijkstra(grid)
+    return if (path != null) {
+        true to lastByte
     } else {
-        return false to lastByte
+        false to lastByte
     }
 }
 
 fun countSteps(grid: Grid): Int {
     var count = 0;
-    for(row in grid.indices) {
-        for(column in grid[row].indices) {
-            if(grid[row][column] == 'O') {
+    for (row in grid.indices) {
+        for (column in grid[row].indices) {
+            if (grid[row][column] == 'O') {
                 count++
             }
         }
@@ -63,12 +85,12 @@ fun countTestGrid() {
     println("TEST GRID: $g")
 }
 
-fun getBytes(name: String): Sequence<Point> {
-    return File("src/days/eightteen/$name.txt")
-        .readLines()
-        .asSequence()
+fun getInput(name: String) = File("src/days/eightteen/$name.txt").readLines()
+
+fun getBytes(input: List<String>): Sequence<Point> {
+    return input.asSequence()
         .map { line -> line.split(',') }
-        .map { l-> l[1].toInt() to l[0].toInt()}
+        .map { l -> l[1].toInt() to l[0].toInt() }
 }
 
 fun getGrid(length: Int): Array<Array<Char>> {
@@ -82,28 +104,28 @@ val left = 0 to -1
 
 val directions = listOf(up, right, down, left)
 
-fun dijistra(grid: Array<Array<Char>>): List<Point>? {
+fun dijkstra(grid: Array<Array<Char>>): List<Point>? {
     val start = 0 to 0
     val end = grid.size - 1 to grid[0].size - 1
 
-    val priorityQueue = PriorityQueue<Pair<Point,Int>>(compareBy({ it.second }))
+    val priorityQueue = PriorityQueue<Pair<Point, Int>>(compareBy({ it.second }))
     priorityQueue.add(start to 0)
     val costSoFar = mutableMapOf<Point, Int>()
     val cameFrom = mutableMapOf<Point, Point>()
 
     while (priorityQueue.isNotEmpty()) {
         val (currentPoint, _) = priorityQueue.remove()
-        if(currentPoint == end) {
+        if (currentPoint == end) {
             break
         }
 
-        for(dir in directions) {
+        for (dir in directions) {
             val nextPoint = currentPoint + dir
             val newCost = costSoFar.getOrDefault(currentPoint, 0) + 1
-            if(nextPoint !in grid || grid[nextPoint] == '#' ) {
+            if (nextPoint !in grid || grid[nextPoint] == '#') {
                 continue
             }
-            if( nextPoint !in cameFrom || newCost < costSoFar[nextPoint]!!) {
+            if (nextPoint !in cameFrom || newCost < costSoFar[nextPoint]!!) {
                 costSoFar[nextPoint] = newCost
                 cameFrom[nextPoint] = currentPoint
                 priorityQueue.add(nextPoint to newCost)
@@ -111,18 +133,17 @@ fun dijistra(grid: Array<Array<Char>>): List<Point>? {
         }
     }
 
-    if(cameFrom[end] == null) {
+    if (cameFrom[end] == null) {
         return null
     }
 
     val path = mutableListOf<Point>()
     var current = end
-    while(current != start) {
+    while (current != start) {
         path.add(current)
         current = cameFrom[current]!!
     }
     path.add(start)
-
     return path
 }
 
@@ -138,40 +159,21 @@ operator fun Grid.contains(p: Point): Boolean {
     return p.row >= 0 && p.column >= 0 && p.row < this.size && p.column < this[0].size
 }
 
-fun binarySearch(): Point {
-    var found: Point = -1 to -1
-    var max = 5000
-    var min = 0
-    var middle = 0
-
-    while(max - min > 1) {
-        middle = (max + min)/2
-        val (result,lastPoint) = arrangeBytesInMemoryFor(middle)
-        found = lastPoint
-        if(result) {
-            min = middle
-        } else {
-            max = middle
-        }
-    }
-
-    return found
-}
-
 fun tests() {
-    val bytesSeq = getBytes("test").take(13)
+    val input = getInput("test")
+    val bytesSeq = getBytes(input).take(13)
 
     val grid = getGrid(7)
 
-    for(byte in bytesSeq) {
-        val (row,column) = byte
+    for (byte in bytesSeq) {
+        val (row, column) = byte
         grid[row][column] = '#'
     }
 
     grid.forEach { println(it.joinToString("")) }
-    val path = dijistra(grid) ?: throw Exception("No path found")
+    val path = dijkstra(grid) ?: throw Exception("No path found")
 
-    for(p in path) {
+    for (p in path) {
         grid[p.first][p.second] = 'O'
     }
     grid.forEach { println(it.joinToString("")) }
@@ -189,7 +191,6 @@ fun tests() {
 //    var middle: Int = 0
 //    while(min < max) {
 //        middle = (max + min)/2
-//        println("min: $min max: $max m:$middle -> ")
 //        if(s == l[middle]) {
 //            return middle
 //        }
